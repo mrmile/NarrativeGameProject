aebc2c8a428f344fbb4b92871c0f2eb516eabb19
mrmile <73582929+mrmile@users.noreply.github.com>
2024-05-15 22:13:01 +0200

    Fixed footsteps script

--- Assets/Scripts/Player/Player_Footsteps.cs
+++ Assets/Scripts/Player/Player_Footsteps.cs
@@ -5,55 +5,35 @@ using UnityEngine;
 public class Player_Footsteps : MonoBehaviour
 {
     public GameObject footstep;
+    bool isWalking = false;
 
     // Start is called before the first frame update
     void Start()
     {
         footstep.SetActive(false);
+        isWalking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
-        {
-            footsteps();
-        }
-
-        if (Input.GetKeyDown("s"))
-        {
-            footsteps();
-        }
-
-        if (Input.GetKeyDown("a"))
-        {
-            footsteps();
-        }
-
-        if (Input.GetKeyDown("d"))
-        {
-            footsteps();
-        }
-
-        if (Input.GetKeyUp("w"))
+        if ((Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) && isWalking == false)
         {
             StopFootsteps();
         }
 
-        if (Input.GetKeyUp("s"))
-        {
-            StopFootsteps();
-        }
 
-        if (Input.GetKeyUp("a"))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
-            StopFootsteps();
-        }
+            footsteps();
 
-        if (Input.GetKeyUp("d"))
-        {
-            StopFootsteps();
+            isWalking = true;
         }
+        else
+        {
+            isWalking = false;
+        }
+        
 
     }
 
