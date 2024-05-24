e6e7e6e38882a96b123f98a42edd24ad5808c5b4
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2024-05-24 14:09:57 +0200

    NPC walk and idle animations

--- Assets/Scripts/Player/Player_Footsteps.cs
+++ Assets/Scripts/Player/Player_Footsteps.cs
@@ -5,33 +5,33 @@ using UnityEngine;
 public class Player_Footsteps : MonoBehaviour
 {
     public GameObject footstep;
-<<<<<<< Updated upstream
+
     bool isWalking = false;
-=======
+
     public GameObject footstep2;
     public GameObject footstep3;
     public GameObject footstep4;
->>>>>>> Stashed changes
 
-    // Start is called before the first frame update
+
+   
     void Start()
     {
         footstep.SetActive(false);
-<<<<<<< Updated upstream
+
         isWalking = false;
-=======
+
         footstep2.SetActive(false);
         footstep3.SetActive(false);
         footstep4.SetActive(false);
->>>>>>> Stashed changes
+
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
+
         if ((Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) && isWalking == false)
-=======
+
         if (Input.GetKey("w"))
         {
             footsteps();
@@ -53,33 +53,33 @@ public class Player_Footsteps : MonoBehaviour
         }
 
         if (Input.GetKeyUp("w"))
->>>>>>> Stashed changes
+
         {
             StopFootsteps();
         }
 
-<<<<<<< Updated upstream
-=======
+
+
         if (Input.GetKeyUp("s"))
         {
             StopFootsteps2();
         }
->>>>>>> Stashed changes
+
 
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
-<<<<<<< Updated upstream
+
             footsteps();
 
             isWalking = true;
-=======
+
             StopFootsteps3();
         }
 
         if (Input.GetKeyUp("d"))
         {
             StopFootsteps4();
->>>>>>> Stashed changes
+
         }
         else
         {
