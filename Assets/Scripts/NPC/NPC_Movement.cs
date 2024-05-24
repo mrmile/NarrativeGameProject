db4d5a1e3577072fb3c92c6eaf8aedb656a49838
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2024-05-24 14:09:57 +0200

    NPC walk and idle animations

--- Assets/Scripts/NPC/NPC_Movement.cs
+++ Assets/Scripts/NPC/NPC_Movement.cs
@@ -12,6 +12,8 @@ public class NPC_Movement : MonoBehaviour
     [SerializeField] Transform lunchLocation;
     [SerializeField] Transform sleepLocation;
 
+    [SerializeField] Animator animator;
+
     bool isMoving;
     int index;
 
@@ -21,34 +23,65 @@ public class NPC_Movement : MonoBehaviour
    
     void Start()
     {
+        animator.SetBool("isIdle", true);
+        animator.SetFloat("X", 0.0f);
+        animator.SetFloat("Y", 0.0f);
 
-        
     }
 
     
     void Update()
     {
-        if(isMoving)
+        if (isMoving)
         {
             CheckNextStep();
 
             Vector3 direction;
-            
+
             direction = (path[index] - transform.position).normalized;
+
+            animator.SetBool("isIdle", false);
+
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+            {
+                animator.SetFloat("Y", 0.0f);
+
+                if (direction.x > 0) animator.SetFloat("X", 1.0f);
+                else animator.SetFloat("X", -1.0f);
+            }
+            else
+            {
+                animator.SetFloat("X", 0.0f);
+
+                if (direction.y > 0) animator.SetFloat("Y", 1.0f);
+                else animator.SetFloat("Y", -1.0f);
+            }
+
             Walk(direction);
-            
+
         }
+        else Idle();
+        
     }
 
     void Walk(Vector3 dir)
     {
+    
         transform.position += dir * speed * Time.deltaTime;
+    }
+    void Idle()
+    {
+        isMoving = false;
+
+        animator.SetFloat("X", 0.0f);
+        animator.SetFloat("Y", 0.0f);
+        animator.SetBool("isIdle", true);
     }
     void CheckNextStep()
     {
         if (index <= 0)
-        {           
-            isMoving = false;
+        {
+            Idle();
         }
         else if (index >= 0 && Vector2.Distance(path[index], transform.position) < minDistanceToNode)
         {            
