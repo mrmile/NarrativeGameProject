2339714af4371ce19eff22b522a670e71d14481a
Oga29 <espagambetovoglan@mail.ru>
2024-06-06 04:34:31 +0200

    Added Intro Cutscene

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGame(string sceneName)
+    private VideoPlayer introVideoPlayer;
+    private Canvas mainMenuCanvas;
+
+    private void Start()
     {
-        SceneManager.LoadScene("MapLevel_0");
+        // Find the VideoPlayer and Canvas in the scene
+        introVideoPlayer = GameObject.Find("VideoPlayer").GetComponent<VideoPlayer>();
+        mainMenuCanvas = GetComponentInParent<Canvas>();
+
+        if (introVideoPlayer != null)
+        {
+            introVideoPlayer.loopPointReached += OnIntroVideoFinished;
+        }
+    }
+
+    public void StartGame()
+    {
+        if (introVideoPlayer != null)
+        {
+            mainMenuCanvas.enabled = false; // Hide the main menu canvas
+            introVideoPlayer.Play();
+        }
+        else
+        {
+            LoadScene("MapLevel_0");
+        }
+    }
+
+    private void OnIntroVideoFinished(VideoPlayer vp)
+    {
+        LoadScene("MapLevel_0");
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 
     public void ExitGame()
